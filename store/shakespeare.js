1f19287d265d385701eb98d012c035e53a1da20a
Andrzej Proczka <aproczka@gmail.com>
2016-11-23 14:19:35 -0700

    Bugfix again on shakespeare.js

--- store/shakespeare.js
+++ store/shakespeare.js
@@ -1,7 +1,6 @@
 const request = require('request')
 
-var Shakespeare = [
-  {
+var Shakespeare = {
     getPlay: function() {
       request('http://www.opensourceshakespeare.org/views/plays/play_view.php?WorkID=allswell&Act=1&Scene=2&Scope=scene&displaytype=print', function (error, response, body) {
         if (!error && response.statusCode == 200) {
@@ -9,22 +8,23 @@ var Shakespeare = [
         }
       });
     },
-    plays: {
-      title:"All's Well That Ends Well",
-      acts:[
-        {
-          title:"Act I",
-          scenes: [
-            {
-              title:"SCENE II. Paris. The KING's palace.",
-              lines:[]
-            }
-          ]
-        }
-      ]
-    }
-  }
-];
+    plays: [
+      {
+        title:"All's Well That Ends Well",
+        acts:[
+          {
+            title:"Act I",
+            scenes: [
+              {
+                title:"SCENE II. Paris. The KING's palace.",
+                lines:[]
+              }
+            ]
+          }
+        ]
+      }
+    ]
+};
 
 Shakespeare.plays[0].acts[0].scenes[0].lines[235] = "*King of France.*  The Florentines and Senoys are by the ears;";
 Shakespeare.plays[0].acts[0].scenes[0].lines[236] = "Have fought with equal fortune and continue";
